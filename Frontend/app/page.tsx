a9020db01183ff72b5efe4e25605809ab3f6b1d0
Md. Nasir Uddin <bsse1310@iit.du.ac.bd>
2026-01-08 23:50:19 +0600

    Different button type added and Gallery Section added

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,10 +1,13 @@
 // src/app/page.tsx
 // import Navbar from "@/components/layout/Navbar";
+import GallerySection from "@/components/common/GallerySection";
+
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      {/* <Navbar /> */}
+      <GallerySection />
+      
     </main>
   );
 }
