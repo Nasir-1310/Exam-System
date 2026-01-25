805cc68026fd5b570025170e16b3c966e2c929e4
Md. Saikat Islam <saikatislam709@gmail.com>
2026-01-25 11:59:08 +0600

    .

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 load_dotenv()
+
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from app.lib.config import settings
